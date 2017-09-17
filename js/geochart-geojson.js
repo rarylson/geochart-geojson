8021dd896b43f036a67cedaa4059fd0e2df170da
Rarylson Freitas <rarylson@vialink.com.br>
2017-09-17 15:27:32 -0300

    Refactor plus showing tooltip
    Tooltip overlay class implemented.

--- js/geochart-geojson.js
+++ js/geochart-geojson.js
@@ -8,8 +8,20 @@ var geochart_geojson = {};
 
 (function(context) { 
 
+context.CONSTANTS = {
+  // zIndex of selected or highlighted features
+  // The selected (click) and highlighted (hover) features must have a zIndex higher than
+  // the other features.
+  selectedZIndex: 999,
+  highlightedZIndex: 1000,
+  // zIndex of the tooltip
+  // The tooltip must have a zIndex higher than the features and the selected and highlighted
+  // features. 
+  tooltipZIndex: 2000
+}
+
 /**
- * Create a GeoChart with GeoJSON support
+ * GeoChart with GeoJSON support
  * @class
  *
  * These charts are very similar to the Google Charts geochart, but with GeoJSON support.
@@ -47,30 +59,36 @@ context.GeoChart = function(container) {
   this.feature_selected_ = null;
 }
 
-context.GeoChart.prototype.CONSTANTS = {
-  // zIndex of selected or highlighted features
-  // The selected (click) and highlighted (hover) features must have a zIndex higher than
-  // the other features.
-  selectedZIndex: 999,
-  highlightedZIndex: 1000
-}
-
 context.GeoChart.prototype.DEFAULT_OPTIONS = {
+  mapsOptions: null,
   mapsBackground: "none",
   mapsControl: false,
   featuresStyle: {
     fillColor: "#f5f5f5",
-    strokeColor: '#dddddd',
+    strokeColor: "#dddddd",
     fillOpacity: 1,
-    strokeWeight: 1,
-    cursor: "default"
+    strokeWeight: 1
   },
   featuresHighlightedStyle: {
     strokeWeight: 3,
     strokeOpacity: 1,
   },
   featuresGradientColors: ["#efe6dc", "#109618"],
-  featuresGradientStrokeColors: ["#d7cfc6", "0e8716"]
+  featuresGradientStrokeColors: ["#d7cfc6", "0e8716"],
+  tooltip: {
+    borderStyle: "solid",
+    borderWidth: "1px",
+    borderColor: "#cccccc",
+    backgroundColor: "#ffffff",
+    padding: "4px",
+    textStyle: {
+      color: "#000000",
+      fontFamily: "Arial",
+      fontSize: "13px",
+      margin: "2px"
+    }
+  },
+  tooltipOffset: 12
 }
 
 // TODO Implement other `mapsBackground` and `mapsControl` options
@@ -98,19 +116,17 @@ context.GeoChart.prototype.getMapsOptions_ = function() {
   return maps_options;
 }
 
-context.GeoChart.prototype.draw = function(data, options) {
+context.GeoChart.prototype.draw = function(data, options={}) {
   this.data_ = data;
-  // XXX This doesn't run a deep copy. So, if someday `DEFAULT_OPTIONS` has objects in its values,
-  // the line above won't work anymore.
+  // FIXME This doesn't run a deep copy.
   // See: https://stackoverflow.com/questions/27936772/how-to-deep-merge-instead-of-shallow-merge
-  var merged_options = Object.assign({}, context.GeoChart.prototype.DEFAULT_OPTIONS, options);
-  this.options_ = merged_options;
+  this.options_ = Object.assign({}, context.GeoChart.prototype.DEFAULT_OPTIONS, options);
 
   var this_ = this;
 
   // Create the Google Maps object
   var maps_options = this.getMapsOptions_();
-  // TODO We could implement custom zooming when mapsBackground = 'none' using custom
+  // TODO We could implement custom zooming when mapsBackground = "none" using custom
   // projections.
   // See: https://groups.google.com/forum/#!topic/google-maps-js-api-v3/AbOHZlLQLCg
   this.map_ = new google.maps.Map(this.container, maps_options);
@@ -136,21 +152,30 @@ context.GeoChart.prototype.draw = function(data, options) {
             max = value;
           }
           feature.setProperty("data-row", row);
+          feature.setProperty("data-id", id);
+          feature.setProperty("data-label", data.getColumnLabel(1));
           feature.setProperty("data-value", value);
         }
         this_.min_ = min;
         this_.max_ = max;
        
-        // Trigger the ready event
-        // See: https://developers.google.com/chart/interactive/docs/dev/events#the-ready-event
-        google.visualization.events.trigger(this_, 'ready', null);
+        // Create the tooltip
+        google.maps.event.addListener(this_.map_, "tooltip-ready", function() {
+          
+          // Trigger the ready event
+          // See: https://developers.google.com/chart/interactive/docs/dev/events#the-ready-event
+          google.visualization.events.trigger(this_, "ready", null);
+        });
+        this_.tooltip_ = new context.Tooltip(this_);
       }
   );
 
   // Define the feature styles
   this.map_.data.setStyle(function(feature) {
     // Default style
-    var style = Object.assign({}, this_.DEFAULT_OPTIONS.featuresStyle);
+    var style = Object.assign(
+        {}, {cursor: "default"},
+        this_.DEFAULT_OPTIONS.featuresStyle);
     
     // Feature with data style
     // Colorize the features with data (using the gradient colors)
@@ -178,17 +203,17 @@ context.GeoChart.prototype.draw = function(data, options) {
       }
 
       style = Object.assign(style, {
-        fillColor: 'rgb(' + fill_color_arr[0] + ',' + fill_color_arr[1] + ',' +
-            fill_color_arr[2] + ')',
-        strokeColor: 'rgb(' + stroke_color_arr[0] + ',' + stroke_color_arr[1] + ',' +
-            stroke_color_arr[2] + ')'
+        fillColor: "rgb(" + fill_color_arr[0] + ", " + fill_color_arr[1] + ", " +
+            fill_color_arr[2] + ")",
+        strokeColor: "rgb(" + stroke_color_arr[0] + ", " + stroke_color_arr[1] + ", " +
+            stroke_color_arr[2] + ")"
       });
 
       // Selected feature style
       if (feature.getProperty("data-selected") === true) {
         style = Object.assign(
             style, this_.DEFAULT_OPTIONS.featuresHighlightedStyle,
-            {zIndex: this_.CONSTANTS.selectedZIndex}
+            {zIndex: context.CONSTANTS.selectedZIndex}
         );
       }
     }
@@ -201,11 +226,14 @@ context.GeoChart.prototype.draw = function(data, options) {
   this.map_.data.addListener("mouseover", function(event) {
     var highlighted_style = Object.assign(
         {}, this_.DEFAULT_OPTIONS.featuresHighlightedStyle,
-        {zIndex: this_.CONSTANTS.highlightedZIndex});
+        {zIndex: context.CONSTANTS.highlightedZIndex});
 
     if (event.feature !== this_.feature_selected_) {    
         this_.map_.data.revertStyle();
         this_.map_.data.overrideStyle(event.feature, highlighted_style);
+    }
+    if (event.feature.getProperty("data-value") !== undefined) {
+      this_.tooltip_.drawTooltip(event.feature, event.latLng);
     }
   });
 
@@ -291,6 +319,108 @@ context.GeoChart.prototype.unselectFeature_ = function() {
     this.feature_selected_.removeProperty("data-selected");
     this.feature_selected_ = null;
   }
+}
+
+/**
+ * Tooltip for GeoChart GeoJSON
+ * @class
+ *
+ * It's an overlay layer to be placed on a Google Maps map.
+ * 
+ * @param {object} geoChart - The GeoChart GeoJSON object where the tooltip will be placed.
+ */
+context.Tooltip = function(geoChart) {
+  this.geo_chart_ = geoChart;
+
+  this.div_ = null;
+  this.id_span_ = null;
+  this.label_span_ = null;
+  this.value_span_ = null;
+
+  this.LatLng = null;
+
+  this.setMap(geoChart.map_);
+}
+
+context.Tooltip.prototype = new google.maps.OverlayView();
+
+context.Tooltip.prototype.onAdd = function() {
+  // Create the main div
+  var div = document.createElement("div");
+  var div_style = {};
+  div_style = Object.assign(
+      {}, {position: "absolute", visibility: "hidden"},
+      this.geo_chart_.options_.tooltip,
+      {zIndex: context.CONSTANTS.tooltipZIndex});
+  delete div_style.textStyle;
+  Object.assign(div.style, div_style);
+
+  var p_style = Object.assign({}, this.geo_chart_.options_.tooltip.textStyle);
+
+  // Create the first inner paragraph
+  var p1 = document.createElement("p");
+  Object.assign(p1.style, p_style);
+  var id_span = document.createElement("span");
+  id_span.style.fontWeight = "bold";
+  p1.appendChild(id_span);
+  div.appendChild(p1);
+
+  // Create the second inner paragraph
+  var p2 = document.createElement("p");
+  Object.assign(p2.style, p_style);
+  var label_span = document.createElement("span");
+  var value_span = document.createElement("span");
+  value_span.style.fontWeight = "bold";
+  p2.appendChild(label_span);
+  p2.appendChild(document.createTextNode(": "));
+  p2.appendChild(value_span);
+  div.appendChild(p2);
+   
+  this.div_ = div;
+  this.id_span_ = id_span;
+  this.label_span_ = label_span;
+  this.value_span_ = value_span;
+
+  this.getPanes().overlayLayer.appendChild(div);
+}
+
+context.Tooltip.prototype.draw = function() {
+  // Do not draw nothing at first
+  // Only trigger an event
+  google.maps.event.trigger(this.map_, 'tooltip-ready');
+}
+
+context.Tooltip.prototype.drawTooltip = function(feature, latLng) {
+  // Calculate positioning
+  var s = this.geo_chart_.options_.tooltipOffset;
+  var px = this.getProjection().fromLatLngToDivPixel(latLng);
+  var w = this.div_.offsetWidth;
+  var h = this.div_.offsetHeight;
+  var top = 0;
+  var left = 0;
+
+  // Start with div up and left
+  top = px.y - s - h;
+  left = px.x - s - w;
+  // Change div side if necessary
+  if (top < 0) {
+    top = px.y + s;
+  }
+  if (left < 0) {
+    left = px.x + s;
+  }
+
+  // Set text and positioning
+  this.id_span_.innerText = feature.getId();
+  this.label_span_.innerText = feature.getProperty("data-label");
+  this.value_span_.innerText = feature.getProperty("data-value");
+  this.div_.style.top = top + 'px';
+  this.div_.style.left = left + 'px';
+  this.div_.style.visibility = "visible";
+}
+
+context.Tooltip.prototype.undrawTooltip = function() {
+  this.div_.style.visibility = "hidden";
 }
 
 })(geochart_geojson);
